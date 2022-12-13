2f480bb9edd52f081899e590af3fc2b09e319936
Wenshuai Hou <wenhoujx@gmail.com>
2022-12-13 14:23:00 -0500

    remove leading whitespace.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# wen-vscode README
+# wen-vscode
 
 this is wenhoujx's vscode extension, i come from emacs where i have the freedom to write a quick `def` to customize my editor. This repo is where i do the same thing. It's a dumping ground of my customized commands.
--- extension.js
+++ extension.js
@@ -1,34 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const CommandId = {
+	REMOVE_LEADING_WHITESPACE: 'wen.remove_leading_whitespace',
+}
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+
+function removeLeadingWhitespace(editor) {
+	const remove = (text) => {
+		if (text.startsWith(" ")) {
+			return text.trimStart()
+		} else {
+			//if no whitespace, add 1 whitespace 
+			return " " + text
+		}
+	}
+	const { text, range } = editor.document.lineAt(editor.selection.active.line)
+	editor.edit(edit => edit.replace(range, remove(text)));
+
+}
+
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "wen-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('wen-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from wen-vscode!');
-	});
+	let disposable = vscode.commands.registerTextEditorCommand(
+		CommandId.REMOVE_LEADING_WHITESPACE,
+		(editor) => removeLeadingWhitespace(editor));
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
